76336a3a0c1321623c1fb6caba8935ee393175b0
Michael Diaz <mdiaz11@uncc.edu>
2022-03-21 19:41:03 -0400

    Added templates folder to house HTML pages. Added some routes to the test python file. This is mainly to show how it works. Added a "home.htm" that will be the homepages. Once I get a decent look, I'll create a template for the rest of the site.

--- test.py
+++ test.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask import render_template
+import re
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "testtemp.html",
+        name=name
+    )
+
+
+
+@app.route("/testarea")
+def testTemp():
+    return render_template(
+        "home.html"
+    )
