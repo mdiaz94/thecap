14756fdcdb6bbcfc81aa6bb15a4ebe75b4cfe8bd
Tallis14 <89754969+Tallis14@users.noreply.github.com>
2022-03-23 17:56:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ connection = sqlite3.connect('Users.db')
 cursor = connection.cursor()
 cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Users
-              (Username, password)''')
+              (ID,Username, password)''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS Bookmarks
-              (Username, bookmark)''')
+              (ID, bookmark)''')
 connection.commit()
 connection.close()
