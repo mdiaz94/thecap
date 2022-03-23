31cce5b384a9776d0e8a87c4182cd0e67e0fbf02
John Doe <bfoste40@uncc.edu>
2022-03-23 17:07:33 -0400

    A little sqlite db
    made a model for a db and ran it

--- test.py
+++ test.py
@@ -2,8 +2,20 @@ from flask import Flask
 from flask import render_template
 import re
 import datetime
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///researchers.db'
+#initialize db
+db = SQLAlchemy(app)
+
+#db model
+class Researchers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @app.route("/")
 def home():
