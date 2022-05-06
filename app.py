a94273fa76d60f1f8ec341d68ed7ccddcc7262f1
Tristan Allison <tallison2001@gmail.com>
2022-05-05 23:43:31 -0400

    Bookmark function

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
+from pickle import TRUE
+from turtle import right
+from types import NoneType
 from typing import Any
-from flask import Flask,redirect, session, url_for
+from flask import Flask, Request,redirect, session, url_for
 from flask import render_template
 from flask import request
+import re
+import datetime
 import sqlite3
+
+import urllib.parse
+import models
+import json
+import requests
 import arxiv
 from datetime import datetime
-import string
-import random
-import requests
-import json
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 datatwo = Any
-searchResults = Any
+search = Any
 directAPI = "682084898b02a949777e0b81f9943e3d"
 
 
 @app.route("/")
 def home():
-    return redirect("index", code=302)
+    return redirect("/index", code=302)
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
@@ -38,154 +43,61 @@ def testTemp():
 @app.route("/index", methods= ['POST', 'GET'])
 def index():
     if request.method == "POST":
-        global searchResults
+        global search
         search = request.form['search']
         search = search.replace(" ", " AND ")
         print(search)
         resultBuilder = ""
-        pageBuilder = ''
         i = 0
         pageCounter = 1
-        searchResults = arxiv.Search (
+        search = arxiv.Search (
                 query = search,
-                max_results = 100
+                max_results = 50
         )
-        for result in searchResults.results():
+        for result in search.results():
             try:
-                result.journal_ref + "test string"
+                tester = result.journal_ref + "test string"
                 publication = result.journal_ref
             except:
-                publication = 'arXiv preprint'
+                publication = 'Unknown Journal'
             publishdate = result.published.strftime('%d %B, %y')
-            authorstring = ""
-            for author in result.authors:
-                authorstring = authorstring + author.name + ", "
-            authorstring = authorstring[:-2]
             resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) + '" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="' + 
-            result.entry_id + '">' + result.title + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + authorstring + '</h6><h6 class="card-subtitle mb-2 text-muted">' + publication + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + publishdate + 
-            '</right></h6> </div><a href="/addbookmark?id=' + result.entry_id + '" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</a></div>')
+            result.entry_id + '">' + result.title + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + publication + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + publishdate + '</right></h6> </div><a href="/addbookmark?id=' + result.entry_id + '" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</a></div>')
             #resultBuilder = resultBuilder + '<p><a href="https://doi.org/' + datatwo[i]['prism:doi'] + '">' + datatwo[i]['dc:title'] + '</a></p>'
             i = i+1
             if (i % 10 == 0):
-                pageBuilder = pageBuilder + '<li class="page-item"><a class="page-link" href="javascript:;" onclick="showPage(' + str(pageCounter) + ')">' + str(pageCounter) + '</a></li>'
                 pageCounter = pageCounter + 1
-                
-        if pageCounter == 1:
-            pageBuilder = ""
-        return render_template("results.html", search=request.form['search'], results=resultBuilder, pages=pageBuilder, maxPageNumber=pageCounter)
+
+        return render_template("results.html", search=request.form['search'], results=resultBuilder)
     return render_template(
         "index.html"
     )
-
-@app.route("/researcher-search", methods = ['POST','GET'])
-def researcher_search():
-    if request.method == 'POST':
-        search = request.form['search']
-        resultBuilder = ""
-        pageBuilder = ''
-        pageCounter = 1
-        i = 0
-        r = requests.get("https://pub.orcid.org/v3.0/expanded-search/", params={"q":search,"rows":"100"}, headers={"Accept":"application/json"})
-        data = json.loads(r.content)
-        for researcher in data['expanded-result']:
-            #if statement is needed because not all researchers are active and some appraently don't have name in the orcid database
-            if (researcher['institution-name'] and researcher['given-names'] and researcher['family-names'] ):
-                fullName = researcher['given-names'] + " " + researcher['family-names']
-                queryBuilder = "researchers?name=" + fullName + "&institution-name=" + researcher['institution-name'][0] + "&orcid-id=" + researcher['orcid-id']
-                resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) + '" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="' + 
-                queryBuilder + '" target="_blank">' + fullName + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + researcher['institution-name'][0] + '</h6></div></div>')
-                i = i + 1
-                if (i % 10 == 0):
-                    pageBuilder = pageBuilder + '<li class="page-item"><a class="page-link" href="javascript:;" onclick="showPage(' + str(pageCounter) + ')">' + str(pageCounter) + '</a></li>'
-                    pageCounter = pageCounter + 1
-                if pageCounter == 1:
-                    pageBuilder = ""
-        return render_template ("researcher-results.html", search=request.form['search'], results=resultBuilder, pages=pageBuilder, maxPageNumber=pageCounter)
-    else:
-        return render_template ("index.html")
-
 
 @app.route("/filter", methods= ['POST', 'GET'])
 def filter():
     if request.method == "POST":
-        global searchResults
+        global datatwo
+        filteredData = datatwo
         if request.form['Date'] == "one":
             def sortFunc(e):
-                return e.published
-            sorted_results = list(searchResults.results())
-            sorted_results.sort(key=sortFunc, reverse = True)
-            i = 0
-            pageCounter = 1
-            resultBuilder = ""
-            pageBuilder = ''
-        for result in sorted_results:
-            try:
-                result.journal_ref + "test string"
-                publication = result.journal_ref
-            except:
-                publication = 'arXiv preprint'
-            publishdate = result.published.strftime('%d %B, %y')
-            authorstring = ""
-            for author in result.authors:
-                authorstring = authorstring + author.name + ", "
-            authorstring = authorstring[:-2]
-            resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) + '" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="' + 
-            result.entry_id + '">' + result.title + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + authorstring + '</h6><h6 class="card-subtitle mb-2 text-muted">' + publication + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + publishdate + 
-            '</right></h6> </div><a href="/addbookmark?id=' + result.entry_id + '" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</a></div>')
-            #resultBuilder = resultBuilder + '<p><a href="https://doi.org/' + datatwo[i]['prism:doi'] + '">' + datatwo[i]['dc:title'] + '</a></p>'
-            i = i+1
-            if (i % 10 == 0):
-                pageBuilder = pageBuilder + '<li class="page-item"><a class="page-link" href="javascript:;" onclick="showPage(' + str(pageCounter) + ')">' + str(pageCounter) + '</a></li>'
-                pageCounter = pageCounter + 1
-                
-        if pageCounter == 1:
-            pageBuilder = ""
-        return render_template("results.html", results=resultBuilder, pages=pageBuilder, maxPageNumber=pageCounter)
-    return render_template(
-        "index.html"
-    )
-@app.route("/filter1", methods= ['POST', 'GET'])
-def filter1():
-    if request.method == "POST":
-        global searchResults
+                return e['prism:coverDate']
+            filteredData.sort(key=sortFunc, reverse = True)
         i = 0
         pageCounter = 1
         resultBuilder = ""
-        pageBuilder = ''
-        
-        for result in searchResults.results():
-            try:
-            
-                result.journal_ref + "test string"
-                publication = result.journal_ref
-            except:
-                publication = 'arXiv preprint'
-            publishdate = result.published.strftime('%d %B, %y')
-            authorstring = ""
-            for author in result.authors:
-                authorstring = authorstring + author.name + ", "
-            authorstring = authorstring[:-2]
-            #Loops through the URLS and webscrapes all subjects, takes a good minunte for it to happen
-            page=requests.get(result.entry_id)
-            soup=BeautifulSoup(page.text,"html.parser")
-            quotes=soup.find("span",attrs={"class" : "primary-subject"})
-
-            resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) +'" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="' + 
-            result.entry_id + '">' + result.title + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + authorstring + '</h6><h6 class="card-subtitle mb-2 text-muted">' + publication + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + publishdate + '</right></h6>' + '<h6 class="card-subtitle mb-2 text-muted"><right>Subject: '+ str(quotes.text) +
-            '</right></h6> </div><a href="/addbookmark?id=' + result.entry_id + '" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</a></div>') 
+        for data in filteredData:
+            resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) + '" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="https://doi.org/' + 
+            filteredData[i]['prism:doi'] + '">' + filteredData[i]['dc:title'] + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' + filteredData[i]['prism:publicationName'] + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + filteredData[i]['prism:coverDate'] + '</right></h6> </div><button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</button></div>')
             #resultBuilder = resultBuilder + '<p><a href="https://doi.org/' + datatwo[i]['prism:doi'] + '">' + datatwo[i]['dc:title'] + '</a></p>'
             i = i+1
             if (i % 10 == 0):
-                pageBuilder = pageBuilder + '<li class="page-item"><a class="page-link" href="javascript:;" onclick="showPage(' + str(pageCounter) + ')">' + str(pageCounter) + '</a></li>'
                 pageCounter = pageCounter + 1
-                
-        if pageCounter == 1:
-            pageBuilder = ""
-            
-        return render_template("results.html", results=resultBuilder, pages=pageBuilder, maxPageNumber=pageCounter)
+
+        return render_template("results.html", search=search, results=resultBuilder)
     return render_template(
         "index.html"
     )
+
 @app.route('/search', methods = ['POST', 'GET'])
 def searchFuture():
     #not using this code rn
@@ -270,8 +182,7 @@ def logout():
 '''start of Topics page'''
 @app.route('/Topics' , methods=['GET','POST'])
 def Topics():
-        string.ascii_letters
-        search = random.choice(string.ascii_letters)
+        search = 'a'
         print(search)
         resultBuilder = ""
         search = arxiv.Search (
@@ -283,15 +194,38 @@ def Topics():
             print(result.title)
             resultBuilder = (resultBuilder + '<div class="card' + " page" + '" style="width: 70%;"><div class="card-body"><h5 class="card-title"><a href="' + 
             result.entry_id + '">' + result.title + '</a></h5><h6 class="card-subtitle mb-2 text-muted">' +  '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' +  '</right></h6> </div><button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-bookmark"></span> Bookmark</button></div>')
-        
-        print(resultBuilder)
+            
         return render_template("Topics.html", search=search, results=resultBuilder)
         #return resultBuilder
 
             
 
 
+'''bookmark page'''
+@app.route("/addbookmark")
+def addBookmark():
+    if request.method == 'GET':
+        if session['logged_in'] == False:
+            return render_template("index.html" ) 
+        conn = sqlite3.connect("Users.db")
+        c = conn.cursor()
+        username = (session['Username'])
+        book = str(request.url)
+        book = book.split("?id=")
+        link = book[1]
+        link = urllib.parse.unquote_plus(link)
+        c.execute("SELECT bookmark FROM bookmark WHERE bookmark = '%s' AND name = '%s'"%(link,username))
+        temp = c.fetchone()
+        print(temp)
+        if(temp is None):
+            c.execute("INSERT INTO bookmark VALUES('%s','%s')"%(link,username))
+        conn.commit()
+        conn.close()
+    return render_template(
+        "bookmarks.html"
+    )
 
+'''bookmark to database'''
 @app.route("/bookmarks")
 def bookmark():
     return render_template(
@@ -301,56 +235,8 @@ def bookmark():
 '''end of Topics page'''
 @app.route("/researchers")
 def research():
-    args = request.args
-    args = args.to_dict()
-    researcherName = args.get('name')
-    researcherInstitution = args.get('institution-name')
-    orcid = args.get('orcid-id')
-    resultBuilder = ""
-    pageBuilder = ''
-    pageCounter = 1
-    i = 0
-    noArticles = ""
-    orcidURL = "https://pub.orcid.org/v3.0/" + orcid + "/works"
-    r=requests.get(orcidURL, params={}, headers={"Accept":"application/json"})
-    data = json.loads(r.content)
-    for research in data['group']:
-        articleTitle = research['work-summary'][0]['title']['title']['value']
-        try:
-            journalTitle = research['work-summary'][0]['journal-title']['value']
-        except:
-            journalTitle = ""
-        try:
-            articleURL = research['work-summary'][0]['url']['value']
-        except:
-            articleURL = ""
-        publishDate = ""
-        try:
-            publishDate = publishDate + research['work-summary'][0]['publication-date']['year']['value']
-        except: pass
-        try:
-            publishDate = publishDate + "-" + research['work-summary'][0]['publication-date']['month']['value']
-        except: pass
-        try:
-            publishDate = publishDate + "-" + research['work-summary'][0]['publication-date']['day']['value']
-        except: pass
-        if articleURL == "":
-            articleURL = articleTitle
-        else:
-            articleURL = '<a href="' + articleURL + '">' + articleTitle + '</a>'
-        resultBuilder = (resultBuilder + '<div class="card' + " page" + str(pageCounter) + '" style="width: 70%;"><div class="card-body"><h5 class="card-title">' + articleURL 
-        + '</h5></h6><h6 class="card-subtitle mb-2 text-muted">' + journalTitle + '</h6><h6 class="card-subtitle mb-2 text-muted"><right>Date Published: ' + publishDate + 
-            '</right></h6> </div></div>')
-        i = i + 1
-        if (i % 10 == 0):
-            pageBuilder = pageBuilder + '<li class="page-item"><a class="page-link" href="javascript:;" onclick="showPage(' + str(pageCounter) + ')">' + str(pageCounter) + '</a></li>'
-            pageCounter = pageCounter + 1
-        if pageCounter == 1:
-            pageBuilder = ""
-    if resultBuilder == "":
-        noArticles = '1'
     return render_template(
-        "research.html", researcherName = researcherName, researcherInstitution = researcherInstitution, results=resultBuilder, pages=pageBuilder, maxPageNumber=pageCounter, noArticles=noArticles
+        "research.html"
     )
 
 
--- models.py
+++ models.py
@@ -4,9 +4,10 @@ from flask import render_template
 import sqlite3
 connection = sqlite3.connect('Users.db')
 cursor = connection.cursor()
+
 cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY,name TEXT NOT NULL,password TEXT);")
 cursor.execute("CREATE TABLE IF NOT EXISTS researchers (researcher_id INTEGER PRIMARY KEY,name TEXT NOT NULL);")
 cursor.execute("CREATE TABLE IF NOT EXISTS publications (publication_id INTEGER PRIMARY KEY,title TEXT,author_id INTEGER, FOREIGN KEY(author_id) REFERENCES researchers(researcher_id));")
-cursor.execute("CREATE TABLE IF NOT EXISTS bookmarks (DOI INTEGER PRIMARY KEY,name TEXT NOT NULL);")
+cursor.execute("CREATE TABLE IF NOT EXISTS bookmark (bookmark TEXT NOT NULL,name TEXT NOT NULL);")
 connection.commit()
 connection.close()
